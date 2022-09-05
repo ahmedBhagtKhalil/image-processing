7ff73f6f349dad41329995d603626fea53b08684
otventures\ahmed.bahgat <ahmed.bahgat@linkdev.com>
2022-09-05 19:20:14 +0200

    commit 3

--- src/server.ts
+++ src/server.ts
@@ -30,7 +30,7 @@ import { Router, Request, Response } from "express";
   /**************************************************************************** */
 
   app.get("/filteredimage", async (req: Request, res: Response) => {
-    const image_url = req.query.image_url.tostring();
+    const image_url = req.query.image_url.toString();
     if (!image_url) res.status(400).send("image url is required");
     const filteredimage = await filterImageFromURL(image_url);
     res.status(200).sendFile(filteredimage, () => {
